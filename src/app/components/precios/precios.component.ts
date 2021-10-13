74d2b6697e248ff31f01eeea248a85fa7732f55e
Agustin Biagi <agustin.biagi@gmail.com>
2021-10-12 22:53:07 -0300

    first commit

---
+++ src/app/components/precios/precios.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-precios',
+  template: `
+    <p>
+      precios works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class PreciosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/protegida/protegida.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-protegida',
+  template: `
+    <p>
+      protegida works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ProtegidaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
